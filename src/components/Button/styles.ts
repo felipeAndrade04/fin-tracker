3382bdc77a45b69ac5e8c658a40963d602edd7d8
Felipe Andrade <flpandrade2011@gmail.com>
2020-12-05 13:14:21 -0300

    Add button component

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.TouchableOpacity`
+  width: 90%;
+  height: 60px;
+  border-radius: 8px;
+  background: #232129;
+  margin-top: 20px;
+
+  align-items: center;
+  justify-content: center;
+`;
+
+export const TextButton = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+  color: #E6E5EC;
+`;
