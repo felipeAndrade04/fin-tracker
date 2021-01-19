4f1222863941411d962885490ac0b5a9a14a086b
Felipe Andrade <flpandrade2011@gmail.com>
2021-01-19 20:36:28 -0300

    Alterando estilos do componente de input

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,21 +1,35 @@
-import styled from 'styled-components/native';
+import styled, {css} from 'styled-components/native';
+
+interface UnderlineProps {
+  isFocused: boolean;
+}
 
 export const Container = styled.View`
-  width: 90%;
-  height: 60px;
-  padding: 0 8px;
-  background: #fff;
-  border-radius: 8px;
-  margin-bottom: 20px;
-  border-width: 2px;
-  border-color: #232129;
+  position: relative;
 
   flex-direction: row;
   align-items: center;
+  margin-bottom: 30px;
 `;
 
 export const TextInput = styled.TextInput`
   flex: 1;
-  color: #000;
-  font-size: 18px;
+  color: #46494c;
+  font-size: 20px;
+  margin-left: -1%;
+`;
+
+export const Underline = styled.View<UnderlineProps>`
+  position: absolute;
+  height: 1.5px;
+  width: 100%;
+  bottom: 0;
+  left: 0;
+  background: #46494c;
+
+  ${(props) =>
+    props.isFocused &&
+    css`
+      background: #529471;
+    `}
 `;
