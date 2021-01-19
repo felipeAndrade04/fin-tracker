bb7d8198ab112bf916f96a2448c507d7e64f0278
Felipe Andrade <flpandrade2011@gmail.com>
2021-01-19 20:35:29 -0300

    Alterando estrutura do componente de input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, {useState, useCallback} from 'react';
 
 import {TextInputProps} from 'react-native';
 
 import {Container, TextInput, Underline} from './styles';
 
 const Input: React.FC<TextInputProps> = (props) => {
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+  }, []);
+
   return (
     <Container>
-      <TextInput {...props} />
-      <Underline />
+      <TextInput
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        placeholderTextColor="#46494c"
+        {...props}
+      />
+      <Underline isFocused={isFocused} />
     </Container>
   );
 };
